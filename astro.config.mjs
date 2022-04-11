9c930e6a9ef454f3fed344cb77f833b18a7786b5
AtomicFeasT <atomic-feast@hotmail.com>
2022-04-10 22:24:08 -0300

    refactor(astro.config.mjs): removed site option

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
-    base: '/space-tourism/',
-    site: 'https://atomicfeast.github.io/'
+    base: '/space-tourism/'
 });
 
 
